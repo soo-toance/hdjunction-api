3117f1ead1af843b004868a6cbe04b21d2a2c651
soobin.lee <soo.toance@gmail.com>
2021-02-10 20:53:43 +0900

    최초 커밋

---
+++ src/main/java/api/hdjunction/hdjunctionapi/HdjunctionApiApplication.java
@@ -0,0 +1,13 @@
+package api.hdjunction.hdjunctionapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HdjunctionApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HdjunctionApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/api/hdjunction/hdjunctionapi/HdjunctionApiApplicationTests.java
@@ -0,0 +1,13 @@
+package api.hdjunction.hdjunctionapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HdjunctionApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
